cc2c4ced97845f9d346bd2cb0e350d8a7f2ee57d
ajvln0217 <127316935+ajvln0217@users.noreply.github.com>
2023-03-14 21:46:05 +0800

    Update code.php

--- admin/code.php
+++ admin/code.php
@@ -215,6 +215,18 @@ if (isset($_POST['add_category'])) {
     $zip = mysqli_real_escape_string($conn, $_POST['zip']);
     $ip_add = getIPAddress();
     $token = md5(rand());
+    
+    function getIPAddress(){
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+          $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+          $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        else{
+          $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+      }
 
 
 
